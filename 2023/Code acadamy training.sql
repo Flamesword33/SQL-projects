d187084d9cceb94980a2b30e10acb02d5fdeff59
Nathan Pelletier <nathantylerpelletier@gmail.com>
2023-05-15 16:09:58 -0600

    Decided to reformat code
    Structured with new line and indentation how I would if statements were to balloon. Also took out comments in front of queries.

--- 2023/Code acadamy training.sql
+++ 2023/Code acadamy training.sql
@@ -1,55 +1,80 @@
 --Lesson 1 part 1/15
 -- The * star character selects everything from the tables given
---SELECT * FROM movies;
+SELECT * 
+  FROM movies;
 
 --part 2
---SELECT name, genre FROM movies;
---SELECT name, genre, year FROM movies;
+SELECT name, genre 
+  FROM movies;
+SELECT name, genre, year 
+  FROM movies;
 
 --part 3
 --AS renames a table or column
---SELECT name AS "title" FROM movies;
---SELECT imdb_rating AS 'IMDb' FROM movies;
+SELECT name AS "title" 
+  FROM movies;
+SELECT imdb_rating AS 'IMDb' 
+  FROM movies;
 
 --part 4
 --DISTINCT removes duplicate entries FROM the results
---SELECT DISTINCT genre FROM movies;
---SELECT DISTINCT year FROM movies;
+SELECT DISTINCT genre 
+  FROM movies;
+SELECT DISTINCT year 
+  FROM movies;
 
 --part 5
 --WHERE is sql's version of if, it restricts the query
---SELECT * FROM movies WHERE imdb_rating < 5;
---SELECT * FROM movies WHERE year > 2014;
+SELECT * 
+  FROM movies 
+    WHERE imdb_rating < 5;
+SELECT * 
+  FROM movies 
+    WHERE year > 2014;
 
 --part 6
 --LIKE creates a pattern to find. It is a more flexible equals
 --  % represents 0-multiple characters
 --  _ represents 1 character
---SELECT * FROM movies WHERE name LIKE 'Se_en';
---SELECT * FROM movies WHERE name LIKE "%vatar%";
+SELECT * 
+  FROM movies 
+    WHERE name LIKE 'Se_en';
+SELECT * 
+  FROM movies 
+    WHERE name LIKE "%vatar%";
 
 --part 7
 --This part teaches the % opperator on LIKE
---SELECT * FROM movies WHERE name LIKE "%man%";
---SELECT * FROM movies WHERE name LIKE 'The%';
+SELECT * 
+  FROM movies 
+    WHERE name LIKE "%man%";
+SELECT * 
+  FROM movies 
+    WHERE name LIKE 'The%';
 
 --part 8
 --IS NULL returns NULL entries in the query
 --IS NOT NULL returns entries in the query
---SELECT name FROM movies WHERE imdb_rating IS NULL;
+SELECT name 
+  FROM movies 
+    WHERE imdb_rating IS NULL;
 
 --part 9
 --BETWEEN a way to combine > and < in one statement that works on numbers and letters. 
 --  Note BETWEEN 'A' AND 'J' will return Andy but not Judy, 
 --  it will return J though.
---SELECT * FROM movies WHERE name BETWEEN 'D' AND 'G';
---SELECT * FROM movies WHERE year BETWEEN 1970 and 1979;
+SELECT * 
+  FROM movies 
+    WHERE name BETWEEN 'D' AND 'G';
+SELECT * 
+  FROM movies 
+    WHERE year BETWEEN 1970 and 1979;
 
 --part 10
 --AND finds info true to both search parameters
-/*
 SELECT *
-FROM movies
-WHERE year < 1985
-   AND genre = 'horror';
-*/
+  FROM movies
+    WHERE year < 1985
+      AND genre = 'horror';
+
+
